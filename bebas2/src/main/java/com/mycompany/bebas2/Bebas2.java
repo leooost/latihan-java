86e4ffa2f4aa6f773d9e4ebad62f9e06f8f820b1
leooost <tambunanleonardo258@gmail.com>
2024-03-21 17:08:49 +0700

    latihan java

---
+++ bebas2/src/main/java/com/mycompany/bebas2/Bebas2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bebas2;
+
+/**
+ *
+ * @author hp
+ */
+public class Bebas2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ bebas2/src/main/java/com/mycompany/bebas2/var2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bebas2;
+
+/**
+ *
+ * @author hp
+ */
+public class var2 {
+    public static void main(String[] args) {
+        
+        
+       int jarijari;
+       double keliling;
+       final double PI=3.14;
+       
+       jarijari = 30;
+       
+       keliling= 2*PI*jarijari;
+       System.out.println("keliling lingkaran = "+keliling);
+      System.out.println("============================");
+      
+     
+        
+    }
+}
