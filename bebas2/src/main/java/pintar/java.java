054c8f81fd80400deecffcf05aeb5fca81f748b1
leoost <tambunanleonardo258@gmail.com>
2024-03-23 22:17:43 +0700

    latihanjava

---
+++ bebas2/src/main/java/pintar/java.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pintar;
+
+/**
+ *
+ * @author hp
+ */
+public class pintar {
+public static void main(string[]args){
+Belajar panggil = new Belajar();
+System.out.println("panggil X : "+panggil.x);
+System.out.println("panggil Y : "+panggil.y);
+}       
+}
---
+++ bebas2/src/main/java/sepeda/java.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pewarisan;
+
+/**
+ *
+ * @author hp
+ */
+class sepeda {
+int kecepatan = 0;
+int gir = 0;
+
+void ubahGir(int pertambahanGir)(
+gir= gir+ pertambahanGir;
+System.out.println("gir:" + gir);
+)
+
+void tambahKecepatan (int pertambahanKecepatan) (
+kecepatan = kecepatan+ pertambahankecepatan;
+System.out.println("Kecepatan:" + kecepatan);
+)
+}
